bda8bc529c1968878939ccd7065b0a0d8d783441
maDU59_ <matthias.ducourant@gmail.com>
2026-06-22 11:10:20 +0200

    Removed useless thing

--- src/client/java/fr/madu59/obe/client/compat/EMFCompat.java
+++ src/client/java/fr/madu59/obe/client/compat/EMFCompat.java
@@ -83,7 +83,6 @@ public class EMFCompat {
             EMFAnimationEntityContext.reset();
             ETFRenderContext.endSpecialRenderOverlayPhase();
             ETFRenderContext.reset();
-            EMFAnimationEntityContext.setCurrentEntityOnShoulder(false);
         }
     }
 }
