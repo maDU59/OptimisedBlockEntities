dd711494f515489afc98ded0f4404fed76ce9462
maDU59_ <matthias.ducourant@gmail.com>
2026-07-07 18:15:27 +0200

    Tried to fix 1-frame invisibility on vanilla

--- src/client/java/fr/madu59/obe/client/mixin/renderer/SectionRenderDispatcherMixin.java
+++ src/client/java/fr/madu59/obe/client/mixin/renderer/SectionRenderDispatcherMixin.java
@@ -4,20 +4,18 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-import com.llamalad7.mixinextras.sugar.Local;
-
 import fr.madu59.obe.client.chunk.ChunkTaskHolder;
 
 import org.spongepowered.asm.mixin.injection.At;
 
+import net.minecraft.client.renderer.chunk.SectionMesh;
 import net.minecraft.client.renderer.chunk.SectionRenderDispatcher.RenderSection;
-import net.minecraft.client.renderer.chunk.SectionRenderDispatcher.RenderSection.SectionTask.SectionTaskResult;
 import net.minecraft.core.SectionPos;
 
-@Mixin(RenderSection.CompileTask.class)
+@Mixin(RenderSection.class)
 public class SectionRenderDispatcherMixin {
-    @Inject(method = "doTask", at = @At("RETURN"))
-    private void obe$onTaskDone(CallbackInfoReturnable<SectionTaskResult> ci, @Local SectionPos sectionPos){
-        ChunkTaskHolder.executeTasks(sectionPos);
+    @Inject(method = "setSectionMesh", at = @At("RETURN"))
+    private void obe$onTaskDone(CallbackInfoReturnable<SectionMesh> ci){
+        ChunkTaskHolder.executeTasks(SectionPos.of(((RenderSection) (Object) this).getRenderOrigin()));
     }
 }
