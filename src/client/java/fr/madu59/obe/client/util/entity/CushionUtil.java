fbdefda6726308d491cd5d12367cd966aec4f32d
maDU59_ <matthias.ducourant@gmail.com>
2026-07-19 11:40:49 +0200

    Added cushion model caching

--- src/client/java/fr/madu59/obe/client/util/entity/CushionUtil.java
+++ src/client/java/fr/madu59/obe/client/util/entity/CushionUtil.java
@@ -1,5 +1,8 @@
 package fr.madu59.obe.client.util.entity;
 
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.math.Axis;
 
@@ -16,6 +19,8 @@ import net.minecraft.world.item.DyeColor;
 import net.minecraft.world.phys.Vec3;
 
 public class CushionUtil{
+    private static Map<CacheKey, BlockEntityStateModel> MODEL_CACHE = new ConcurrentHashMap<>();
+
     public static ModelLayerLocation getModelLayerLocation(){
         return ModelLayers.CUSHION;
     }
@@ -25,11 +30,24 @@ public class CushionUtil{
     }
 
     public static BlockEntityStateModel getModel(CushionSnapshot snapshot){
-        PoseStack poseStack = new PoseStack();
-        poseStack.translate(snapshot.pos().subtract(Vec3.atLowerCornerOf(snapshot.blockPos())));
-        poseStack.mulPose(Axis.YP.rotationDegrees(snapshot.rotation().y));
-        poseStack.mulPose(Axis.XP.rotationDegrees(180.0F));
-        poseStack.translate(0.0, -0.25, 0.0);
-        return new BlockEntityStateModel(getModelLayerLocation(), getMaterial(snapshot.color()), poseStack, SettingsManager.OPTIMISED_CUSHIONS.getValue(), null, null);
+
+        double relativeX = snapshot.pos().x - snapshot.blockPos().getX();
+        double relativeY = snapshot.pos().y - snapshot.blockPos().getY();
+        double relativeZ = snapshot.pos().z - snapshot.blockPos().getZ();
+
+        return MODEL_CACHE.computeIfAbsent(new CacheKey(relativeX, relativeY, relativeZ, snapshot.rotation().y, snapshot.color().getId()), (k) -> {
+            PoseStack poseStack = new PoseStack();
+            poseStack.translate(k.relativeX, k.relativeY, k.relativeZ);
+            poseStack.mulPose(Axis.YP.rotationDegrees(k.yRot));
+            poseStack.mulPose(Axis.XP.rotationDegrees(180.0F));
+            poseStack.translate(0.0, -0.25, 0.0);
+            return new BlockEntityStateModel(getModelLayerLocation(), getMaterial(snapshot.color()), poseStack, SettingsManager.CUSHION_AMBIENT_OCCLUSION.getValue(), null, null);
+        });
     }
+
+    public static void clearCache(){
+        MODEL_CACHE.clear();
+    }
+
+    private static record CacheKey(double relativeX, double relativeY, double relativeZ, float yRot, int colorId){}
 }
