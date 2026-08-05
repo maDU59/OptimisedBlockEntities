f74fdc6a52e8acec09abf7b9f2d86037e1e47410
maDU59_ <matthias.ducourant@gmail.com>
2026-08-05 11:17:48 +0200

    Improved containers emptiness detection

--- src/client/java/fr/madu59/obe/client/renderer/blockentity/SpecialBlockEntityRenderingManager.java
+++ src/client/java/fr/madu59/obe/client/renderer/blockentity/SpecialBlockEntityRenderingManager.java
@@ -1,5 +1,7 @@
 package fr.madu59.obe.client.renderer.blockentity;
 
+import java.util.List;
+
 import fr.madu59.obe.client.config.SettingsManager;
 import net.minecraft.client.Minecraft;
 import net.minecraft.world.item.ItemStack;
@@ -13,25 +15,28 @@ import net.minecraft.world.level.block.entity.SignTextSlot;
 public class SpecialBlockEntityRenderingManager {
     public static boolean shouldSkipRendering(BlockEntity be) {
         if(!SettingsManager.MOD_TOGGLE.getValue()) return false;
-        else if(be instanceof SignBlockEntity signBe && SettingsManager.OPTIMISED_SIGNS.getValue()){
-            return isEmpty(signBe);
+        else if(be instanceof SignBlockEntity signBe){
+            return SettingsManager.OPTIMISED_SIGNS.getValue() && isEmpty(signBe);
         }
-        else if(be instanceof BeaconBlockEntity beaconBe && SettingsManager.OPTIMISED_BEACONS.getValue()){
-            return beaconBe.getBeamSections().isEmpty();
+        else if(be instanceof BeaconBlockEntity beaconBe){
+            return SettingsManager.OPTIMISED_BEACONS.getValue() && beaconBe.getBeamSections().isEmpty();
         }
-        else if(be instanceof CampfireBlockEntity campfireBe && SettingsManager.OPTIMISED_CAMPFIRES.getValue()){
-            for(ItemStack itemStack : campfireBe.getItems()){
-                if(itemStack != null && itemStack != ItemStack.EMPTY) return false;
-            }
-            return true;
+        else if(be instanceof CampfireBlockEntity campfireBe){
+            return SettingsManager.OPTIMISED_CAMPFIRES.getValue() && isContainerEmpty(campfireBe.getItems());
         }
-        else if(be instanceof ShelfBlockEntity shelfBe && SettingsManager.OPTIMISED_SHELVES.getValue()){
-            for(ItemStack itemStack : shelfBe.getItems()){
-                if(itemStack != null && itemStack != ItemStack.EMPTY) return false;
-            }
-            return true;
+        else if(be instanceof ShelfBlockEntity shelfBe){
+            return SettingsManager.OPTIMISED_SHELVES.getValue() && isContainerEmpty(shelfBe.getItems());
         }
         return false;
+    }
+
+    private static boolean isContainerEmpty(List<ItemStack> items) {
+        for (int i = 0; i < items.size(); i++) {
+            if (items.get(i) != null && !items.get(i).isEmpty()) {
+                return false;
+            }
+        }
+        return true;
     }
 
     private static boolean isEmpty(SignBlockEntity be){
