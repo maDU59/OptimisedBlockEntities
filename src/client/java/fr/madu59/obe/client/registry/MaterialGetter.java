c6d380fbb71aaea5aa2aaa4dcf9d1114cbb76995
maDU59_ <matthias.ducourant@gmail.com>
2026-08-08 16:12:53 +0200

    Added per-block registry

--- src/client/java/fr/madu59/obe/client/registry/MaterialGetter.java
+++ src/client/java/fr/madu59/obe/client/registry/MaterialGetter.java
@@ -13,11 +13,13 @@ import fr.madu59.obe.client.util.blockentity.DecoratedPotUtil;
 import fr.madu59.obe.client.util.blockentity.ShulkerBoxUtil;
 import fr.madu59.obe.client.util.blockentity.SkullBlockUtil;
 import net.minecraft.resources.Identifier;
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
 
 public class MaterialGetter {
     private static Map<BlockEntityType<?>, Function<BlockState, Identifier>> materialsGetterProvider = new ConcurrentHashMap<>();
+    private static Map<Block, Function<BlockState, Identifier>> perBlockMaterialsGetterProvider = new ConcurrentHashMap<>();
     private static Map<String, Function<BlockState, Identifier>> defaultMaterialsGetterProvider = new ConcurrentHashMap<>();
 
     public static void init(){
@@ -43,19 +45,30 @@ public class MaterialGetter {
         materialsGetterProvider.put(beType, getter);
     }
 
+    public static void register(Block block, Function<BlockState, Identifier> getter){
+        perBlockMaterialsGetterProvider.put(block, getter);
+    }
+
     public static Identifier getMaterial(BlockState state){
         return getMaterial(state, null);
     }
 
     public static Identifier getMaterial(BlockState state, String group){
         if(!state.hasBlockEntity()) return null;
+
+        Block block = state.getBlock();
+        Function<BlockState, Identifier> provider = perBlockMaterialsGetterProvider.get(block);
+        if (provider != null) return provider.apply(state);
+
         BlockEntityType<?> beType = Registry.getBlockEntityType(state);
         if (beType == null) return null;
-        Function<BlockState, Identifier> provider = materialsGetterProvider.get(beType);
+        provider = materialsGetterProvider.get(beType);
         if (provider != null) return provider.apply(state);
+
         if (group == null) group = Registry.getGroup(beType);
         if (group != null) provider = defaultMaterialsGetterProvider.get(group);
         if (provider != null) return provider.apply(state);
+        
         return null;
     }
 }
