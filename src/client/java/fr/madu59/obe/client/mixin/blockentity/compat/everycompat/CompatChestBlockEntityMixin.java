c2a052e34334dfcacea1cc8f9eaa6a50bd34f489
maDU59_ <matthias.ducourant@gmail.com>
2026-08-10 00:18:33 +0200

    Fixed last commit

--- src/client/java/fr/madu59/obe/client/mixin/blockentity/compat/everycompat/CompatChestBlockEntityMixin.java
+++ src/client/java/fr/madu59/obe/client/mixin/blockentity/compat/everycompat/CompatChestBlockEntityMixin.java
@@ -7,17 +7,11 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import fr.madu59.obe.client.renderer.blockentity.ext.BlockEntityExt;
-import fr.madu59.obe.client.renderer.misc.RenderModeManager;
-import fr.madu59.obe.client.renderer.misc.RenderModeManager.RenderMode;
 import net.mehvahdjukaar.every_compat.common_classes.CompatChestBlockEntity;
-import net.minecraft.core.BlockPos;
-import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.entity.BlockEntity;
-import net.minecraft.world.level.block.state.BlockState;
-import noobanidus.mods.lootr.common.block.entity.LootrChestBlockEntity;
 
 @Pseudo
-@Mixin(value = CompatChestBlockEntityMixin.class, remap = false)
+@Mixin(value = CompatChestBlockEntity.class, remap = false)
 public class CompatChestBlockEntityMixin {
     @Inject(method = "<init>", at = @At("TAIL"))
     private void obe$register(CallbackInfo ci){
