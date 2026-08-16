8b1b6580fd655959a93237f4f1f9997d3c6a1724
maDU59_ <matthias.ducourant@gmail.com>
2026-08-16 21:09:38 +0200

    Some improvements

---
+++ src/client/java/fr/madu59/obe/client/mixin/renderer/BlockRenderDispatcherMixin.java
@@ -0,0 +1,44 @@
+package fr.madu59.obe.client.mixin.renderer;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import com.llamalad7.mixinextras.sugar.Local;
+
+import fr.madu59.obe.client.renderer.blockentity.ext.BlockEntityExt;
+import fr.madu59.obe.client.renderer.misc.RenderModeManager;
+import fr.madu59.obe.client.renderer.misc.RenderModeManager.RenderMode;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.block.BlockRenderDispatcher;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.SectionPos;
+import net.minecraft.world.level.block.RenderShape;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.state.BlockState;
+
+@Mixin(BlockRenderDispatcher.class)
+public class BlockRenderDispatcherMixin {
+
+    @WrapOperation(method = "renderBreakingTexture", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/block/state/BlockState;getRenderShape()Lnet/minecraft/world/level/block/RenderShape;"))
+    private RenderShape obe$getRenderShape(BlockState state, Operation<RenderShape> original, @Local BlockPos pos){
+        if(state.hasBlockEntity()){
+            BlockEntity be = Minecraft.getInstance().level.getBlockEntity(pos);
+            BlockEntityExt ext = (BlockEntityExt) be;
+            if(ext != null && ext.isSupported()) {
+                RenderModeManager.updateBlockEntityOnChunkRemesh(ext, SectionPos.of(pos));
+                if(ext.forceEntity()){
+                    return original.call(state);
+                }
+                if(ext.isEnabled() && !ext.hasSpecialRenderer() && ext.renderMode() != RenderMode.TERRAIN){
+                    return original.call(state);
+                }
+                if(ext.isEnabled() && ext.renderMode() == RenderMode.TERRAIN){
+                    return RenderShape.MODEL;
+                }
+            }
+        }
+        return original.call(state);
+    }
+}
