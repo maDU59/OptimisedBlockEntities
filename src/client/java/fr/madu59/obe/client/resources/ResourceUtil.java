9b1a9033643a076d5871160cc02c751ec1eaa9ec
maDU59_ <matthias.ducourant@gmail.com>
2026-08-18 16:49:34 +0200

    Fixed a compat bug

--- src/client/java/fr/madu59/obe/client/resources/ResourceUtil.java
+++ src/client/java/fr/madu59/obe/client/resources/ResourceUtil.java
@@ -6,6 +6,7 @@ import java.util.concurrent.ConcurrentHashMap;
 import com.mojang.blaze3d.vertex.PoseStack;
 
 import fr.madu59.obe.client.model.BlockEntityStateModel;
+import fr.madu59.obe.client.registry.Registry;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.model.geom.ModelLayerLocation;
 import net.minecraft.client.renderer.block.BlockModelShaper;
@@ -75,7 +76,12 @@ public class ResourceUtil{
 
     public static BakedModel getDefaultModel(BlockState blockState){
         BlockModelShaper modelSet = Minecraft.getInstance().getModelManager().getBlockModelShaper();
-        return modelSet.modelByStateCache.getOrDefault(blockState, modelSet.getModelManager().getMissingModel());
+        if(Registry.getGroup(blockState) != null){
+            return modelSet.modelByStateCache.getOrDefault(blockState, modelSet.getModelManager().getMissingModel());
+        }
+        else{
+            return modelSet.getBlockModel(blockState);
+        }
     }
 
     public record ModelCacheKey(ModelLayerLocation modelLayerLocation, BlockState blockState) {}
