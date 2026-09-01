fc66f0ec3fec4a020caf0578d71f8524cb640777
maDU59_ <matthias.ducourant@gmail.com>
2026-09-01 10:29:19 +0200

    Refactored the getDoubleChest method

--- src/client/java/fr/madu59/obe/client/mixin/blockentity/chest/ChestBlockEntityMixin.java
+++ src/client/java/fr/madu59/obe/client/mixin/blockentity/chest/ChestBlockEntityMixin.java
@@ -1,7 +1,6 @@
 package fr.madu59.obe.client.mixin.blockentity.chest;
 
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -10,14 +9,12 @@ import fr.madu59.obe.client.registry.Registry;
 import fr.madu59.obe.client.renderer.blockentity.ext.BlockEntityExt;
 import fr.madu59.obe.client.renderer.misc.RenderModeManager;
 import fr.madu59.obe.client.renderer.misc.RenderModeManager.RenderMode;
+import fr.madu59.obe.client.util.blockentity.ChestUtil;
 import net.minecraft.core.BlockPos;
-import net.minecraft.core.Direction;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.entity.ChestBlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.level.block.state.properties.BlockStateProperties;
-import net.minecraft.world.level.block.state.properties.ChestType;
 
 @Mixin(ChestBlockEntity.class)
 public abstract class ChestBlockEntityMixin{
@@ -36,33 +33,14 @@ public abstract class ChestBlockEntityMixin{
         if(entity.getOpenNess(0.5f) > 0){
             RenderModeManager.setRenderModeDelayed(ext, RenderMode.ENTITY, pos);
 
-            ChestBlockEntity doubleChest = obe$getDoubleChest(level, pos, state);
+            ChestBlockEntity doubleChest = ChestUtil.getOtherHalf(level, pos, state);
             if(doubleChest != null) RenderModeManager.setRenderModeDelayed(doubleChest, RenderMode.ENTITY, pos);
         }
         else{
             RenderModeManager.setRenderModeDelayed(ext, RenderMode.TERRAIN, pos);
 
-            ChestBlockEntity doubleChest = obe$getDoubleChest(level, pos, state);
+            ChestBlockEntity doubleChest = ChestUtil.getOtherHalf(level, pos, state);
             if(doubleChest != null) RenderModeManager.setRenderModeDelayed(doubleChest, RenderMode.TERRAIN, pos);
         }
-    }
-
-    @Unique
-    private static ChestBlockEntity obe$getDoubleChest(final Level level, final BlockPos pos, final BlockState state){
-        ChestType type = state.getValueOrElse(BlockStateProperties.CHEST_TYPE, ChestType.SINGLE);
-        if (type == ChestType.SINGLE) {
-            return null;
-        }
-
-        Direction facing = state.getValueOrElse(BlockStateProperties.HORIZONTAL_FACING, Direction.NORTH);
-        
-        Direction connectedDirection = (type == ChestType.LEFT) ? facing.getClockWise() : facing.getCounterClockWise();
-        BlockPos neighborPos = pos.relative(connectedDirection);
-
-        if (level.getBlockEntity(neighborPos) instanceof ChestBlockEntity neighborChest) {
-            return neighborChest;
-        }
-
-        return null;
     }
 }
