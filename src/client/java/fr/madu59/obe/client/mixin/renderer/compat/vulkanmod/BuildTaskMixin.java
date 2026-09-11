0ec200cc9a8d150790a7aaa0556cddb1679ab951
maDU59_ <matthias.ducourant@gmail.com>
2026-09-11 11:30:10 +0200

    Removed unneeded methods on 26.2 or older

--- src/client/java/fr/madu59/obe/client/mixin/renderer/compat/vulkanmod/BuildTaskMixin.java
+++ src/client/java/fr/madu59/obe/client/mixin/renderer/compat/vulkanmod/BuildTaskMixin.java
@@ -16,27 +16,16 @@ import com.llamalad7.mixinextras.sugar.Local;
 import com.llamalad7.mixinextras.sugar.Share;
 import com.llamalad7.mixinextras.sugar.ref.LocalRef;
 
-import fr.madu59.obe.client.chunk.ChunkTaskHolder;
-import fr.madu59.obe.client.config.SettingsManager;
-import fr.madu59.obe.client.model.BlockEntityStateModel;
 import fr.madu59.obe.client.renderer.blockentity.BlockEntityModelsManager;
 import fr.madu59.obe.client.renderer.blockentity.ext.BlockEntityExt;
-import fr.madu59.obe.client.renderer.entity.MeshableEntityTracker;
-import fr.madu59.obe.client.renderer.entity.MeshableEntityTracker.MeshableEntityData;
-import fr.madu59.obe.client.renderer.entity.ext.EntityExt;
 import fr.madu59.obe.client.renderer.misc.RenderModeManager.RenderMode;
 import fr.madu59.obe.client.util.meshing.SectionMeshingUtil;
-import net.caffeinemc.mods.sodium.client.world.LevelSlice;
-import net.minecraft.client.Minecraft;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.SectionPos;
-import net.minecraft.util.RandomSource;
-import net.minecraft.core.BlockPos.MutableBlockPos;
 import net.minecraft.world.level.block.RenderShape;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 import net.vulkanmod.render.chunk.build.RenderRegion;
-import net.vulkanmod.render.chunk.build.renderer.BlockRenderer;
 import net.vulkanmod.render.chunk.build.task.BuildTask;
 import net.vulkanmod.render.chunk.build.task.CompileResult;
 
@@ -64,40 +53,6 @@ public class BuildTaskMixin {
         BlockEntityExt ext = (BlockEntityExt) be;
         if(ext != null && ext.isEnabled() && (!(ext.forceEntity() || !ext.isSupported() || ext.renderModeDelayed() == RenderMode.ENTITY || ext.renderBoth()) || ext.shouldSkipRendering())) {
             ci.cancel();
-        }
-    }
-
-    @Inject(method = "compile", at = @At(value = "INVOKE", target = "Lnet/vulkanmod/render/chunk/build/RenderRegion;loadBlockStates()V"))
-    private void obe$appendMeshData(CallbackInfo ci, @Local BlockRenderer blockRenderer, @Local LevelSlice slice, @Local(ordinal = 0) BlockPos startPos){
-        if(!SettingsManager.MOD_TOGGLE.getValue()) return;
-
-        SectionPos sectionPos = SectionPos.of(startPos);
-
-        Collection<MeshableEntityData> entitiesData = MeshableEntityTracker.getMeshableEntities(sectionPos);
-        if(entitiesData == null) return;
-
-        MutableBlockPos pos = new MutableBlockPos();
-        MutableBlockPos modelOffset = new MutableBlockPos();
-
-        for(MeshableEntityData data : entitiesData){
-            if(!data.isEnabled()) continue;
-            if(data.level() != Minecraft.getInstance().level){
-                MeshableEntityTracker.deleteInvalidMeshableEntity(data.id(), data.blockPos());
-                continue;
-            }
-            BlockEntityStateModel model = data.getModel();
-            ChunkTaskHolder.addTask(sectionPos, () -> {
-                EntityExt ext = ((EntityExt)Minecraft.getInstance().level.getEntity(data.id()));
-                if(ext != null) ext.renderMode(RenderMode.TERRAIN);
-            });
-            pos.set(data.blockPos());
-
-            int localX = pos.getX() & 15;
-            int localY = pos.getY() & 15;
-            int localZ = pos.getZ() & 15;
-            modelOffset.set(localX, localY, localZ);
-
-            model.emitQuads(blockRenderer.getEmitter(), this.region, pos, region.getBlockState(pos), RandomSource.create(42), (direction) -> false);
         }
     }
 }
